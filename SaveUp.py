de0e058292248b0c443c051d4024cde3f3b7c139
Dewayne Cowles <dewww@users.noreply.github.com>
2018-10-21 05:47:44 -0700

    fix closing popover views

--- SaveUp.py
+++ SaveUp.py
@@ -14,17 +14,17 @@ def sweep_action(sender):
 
 	v['view1'].send_to_back()
 #	v['view1'].hidden = True
-	v['Congradulations'].bring_to_front()
-	v['Congradulations'].hidden = False
-	v['Congradulations'].present('popover',hide_close_button=False)
+	v['Congratulations'].bring_to_front()
+	v['Congratulations'].hidden = False
+	v['Congratulations'].present('popover',hide_close_button=False)
 	
 def reset_view(sender):
 	v['view1'].bring_to_front()
 	v['view1'].hidden = False
 	v['Transactions'].send_to_back()
 	v['Transactions'].hidden = True
-	v['Congradulations'].send_to_back()
-	v['Congradulations'].hidden = True
+	v['Congratulations'].send_to_back()
+	v['Congratulations'].hidden = True
 
 def segmented_action(sender):
 #	if sender.segments[sender.selected_index] == 'meetinggoalsegment':
@@ -32,38 +32,38 @@ def segmented_action(sender):
 	v['view1']['saveuptosegment'].selected_index += 1
 
 data = '''\
-QlpoOTFBWSZTWWWZFPQACVJf4VVQVGd/9b/nna6/79/+QAAAIBAAYAd/B5ZNLsrsUAPuAA
+QlpoOTFBWSZTWW8ludUACVJf4VVQVGd/9b/nna6/79/+QAAAIBAAYAd/B5ZNLsrsUAPuAA
 AewymU1HqntU8U9qnsigyAAAGgAAAADQjQ0AjJTSnqAAAZGQAAAABxkyaGIxNGARgJhAGA
-mmjTI0AwkUyppPU00AAAAAAAANAAAMpRkGmgNADQADQ9QADQAAGgiUTImjSYmhopoBkyA0
-HqZABkaZNpBdHapEVDxo6Prns2USuFD72IVY2bNasIBaBIyFkYqFUYjojZ7OVALNkCRGEI
-EAJFQhEMcdSKOqIgI6lRAtSh3kBCtYlD0UBoQIBDzQBrFCpFpM0RHL8p57gB/P9eeegwS5
-8Pm481kBbQBS3t6W7g75rYdrIvbrbq+MD2K3TyE0MikxIMIamCJVATDGRMIlg2BJFCKNsT
-TZNCMiAVIJUogXBIgb1/vveE4yrVLIEZMuz7PDCWm8QUB0bK3N4bG4pI2jyQW30pq3ngpw
-LYMjFnOHpxkasrg263DaorGYMLDYJIgewxY/NBAI1yHy/O+NGp9BYqVJIMGEIQtZ8YVEuQ
-Bta1IWcGowCJC4N8KyAzBG925YgyhIKaUlSKplTSbQ3CQFkzl9w3pIsrLLH86U8EaEGrRo
-JBII0RojQQ8huKFiCB1R8CQIREhhkL1175364HK9gwtQyGomJsSGnBKhBaqauhkXiAL1Vt
-QymVVadK45x7NOfcYIp2hVKJlK5B5SivlKF7tZye6GxXARGxdKVNJPc0A3RgQLEo6iYyRH
-ZPKdoQaDc2E2uobluGdvJ45DycTiUyU02BoVB1YKTAMC5lO+oQPTd/yst+pE8uN12vaL5p
-50DKh2BewPRNw7jXuvTnpgPUcBnNweQWCENKNeR1VGxB4UOeqXqXLRKadhLAkzrCJKC91C
-Ny1yhc1Y26iypnhbUKhAiBkhHm74vClhnB2nCeYQBGYhrbVeUHwCVQCs3pqVL3AA79OE3S
-64Vrb4HML0AA5MQ58GEtVAoSMQRZN0+2RfookJvIKd7bOp7VK9rC4etEttOAcjvPU+ajJk
-sGG+HPsi28ydiAwUUyjFYAtrpY1WkuiRFKuGISeEIjaLhtU4tQuH33Slb8n1VbgDlGsQi5
-0Rp51yZz2o6WDTilsnmbVtFJlvE3lcDA2hSVsBLneAKErTkDi/WosaxaMjnVcGL0sbiAVp
-O7iVTxXriOGoulptzCtOoDrfMOJoeFfUhHEo8LB+MqGdLNaJGYKI64QWwm6mt33JA4DRAg
-QL25kP4IdzV2Q3js7AhrVHHwOj2aad1uMntRIm3YxKyVEPuA+txjjGKcUZ13G7jmMec8nA
-75jbtfS/UQhZ2TOQTBj0qATE8XdmcOo6y+0XoumcUJhU5qrvIB4apUX1HJ88XZ1usY5ku2
-doUnHDq5te19VoJupGR1Zxzfh7xllyJ8cZuptSdoTzR+MWvXCYR8XdiH5DReBkRN7/tesO
-9QQMGC2Qgg2gUAOyALaZSGEpKSDfp19LBjVlTEBaCBsGA0LFBPKB3cagfIDsH+A5OB6m76
-npNe5+jEuPMrXUqXUDWgG8CZuKB6NckOFXeIaEQp6wSTMClWIBkn9PFAwAwbgFVHRqBcx2
-oFsdnmjIpl3GOzmgEiqewTF/jAkP2Ogg0QKIEciienw8XMu6HQEfYbzrEKoHI1r09Jt1o8
-0NRfHUUeJVAogaZd1DErThOSB9VFMR9fY+JXeg7yIBrdvtM1hVCDfZh6mGRUO8Cae0AdyO
-8NZYsOhDcWIJnmUOfqE1dTOBAYQeBrHBANuR0hdAjZ7KlT3VMl2GaUAOe6Z4PcdNS4C9Oz
-p4DANRjqU9hIVb7BgBm9/EMYXyG6Bg6zAC1QCgwIZGzf1zUaIG25yDilw1sIx7aPOAUDrT
-oscDJnUBKF93GQ4JTXHdsQDvlwL/ZsBOkgYtCiZE0EPdgEi0qiHEvSUeOPAAzoCcRDqQDL
-nTa2r0OfrSSJ77zyFK7qIGYGRs68zNAumKdBTYCZDcl11Shc3GTwHxb6AmsLJDWOYJockq
-Ae5ruWcdtENr2Anak3AGH+6PZnS2poRAuSvg58MBDrvoF02iUPQx9KMR84xCDBgyDCECMk
-QjNaAfh5tMcW/pQDtwO1sJ2ou9V+NwuHiKlgmidxy1HUOSXKskMVRJNGOAA45U/i7kinCh
-IMsyKegA==
+mmjTI0AwkUyqek9TTQAAAAGgAADQAADKUZBpoDQA0AA0PUAA0AABoIlEyJo0jJoaQjQZMg
+ZB6mQAYBNpBdHapEVDxo6Punv2USuFD8mIVY2bNasIBaBIyFkYqFUYjojZ7XGgFmyBIjCE
+CAEioQiGOOpFHVEQEdSogWpQ7iAhWsSh6qA0IEAh5oA1ihUi0maIjl+s89wA9Pr889Rglz
+5fNw5LIC0gClq3u6mFvmviWcm+sW6vjB9it08hNDIpMSDCGpgiVQEwx0TCJYNgSRQijbE0
+2TQjIgFSCVKIFwSIG5f77ngOEq1SyBGTLs+zwwlpvEFAdGytzeGxuKSNo8kFt9Kat54KcC
+2DIxZzh6cZGrK4Nutw2qKxmDCw2CSIHsMWPzQQCNch9P1vjRqeIsVKkkGDCEIWs+MKiXIA
+2takLODUYBEhcG+FZAZgje7csQZQkFNKSpFUyppNobhICwZ6+4b00WFmFj06U78aEGrRoJ
+BII0RojQQ8hylCxBA6Y99IEIiQwyF6q9471cDjewYWoZDUTE3UhqQSoQWamrsZF6gC+VbU
+Mpl1WXSuOcezTn3GCKdoVSiZSuQeUor5She7WcnuhsVwERsXSlTST4dAOWMCBYlHUTGSI7
+J5TrCDQbmwm11Dctvzt5PHIeTgcCmSmmwNCoOrBSYBgXMp31CB6bv+Vlv1Inlxuu17RfNP
+OgZUOwL2B6JuHca916c1MB6TeM5N7xCwQhpRrxOmo2IO+hzVS9S5aJTTsJYEmdYRJQXuoR
+uWuULmrG3UWVM8LahUIEQMkI83fF4UsM4O04TzCAIzENbaryg+ASqAVm9NSpe4AHfpwm6X
+XCtbfA5hegAHJiHPgwlqoFCRiCLJun2yL9FEhN5BTvbZ1PapXtYXD1oltpwDkd56nzUZMl
+gw3w59kW3mTsQGCimUYrAFtdLGq0l0SIpVwxCTwhEbRcNqnFqFw++6Urfk+qrcAco1iEXO
+iNPOuTOe1HSwacUtk8zatopMt4m8rgYG0KStgJc7wBQlacgcX61FjWLRkc6rgxeljcQCtJ
+3cSqeK9cRw1F0tNuYVp1Adb5hxNDwr6kI4lHhYPxlQzpZrRIzBRHXCC2E3U1u+5IHAaIEC
+Be3Mh/BDuauyG8dnYENao4+B0ezTTutxk9qJE27GJWSoh9wH1uMcYxTijOu43ccxjznk4H
+fMbdr6X6iELOyZyCYMelQCYni7szh1HWX2i9F0zihMKnNVd5APDVKi+o5Pni7Ot1jHMl2z
+tCk44dXNr2vqtBN1IyOrOOb8PeMsuRPjjN1NqTtCeaPxi164TCPi7sQ/IaLwMiJvf9r1h3
+qCBgwWyEEG0CgB2QBbTKQwlJSQb9OvpYMasqYgLQQNgwGhYoJ5QOzhUD6Adg+gOLge1u+1
+6DlfFcsPIrXJU2qBrQDcBM3FAw9WuSG/B3CGhEKe4EkxApViAZJ63ggYAYNwCqjo1AuY7U
+C2w80JFMewvs5IBIqnvExfRgSH8nOQaIFECORRPZ4ODmXdDnCPvOU6hCqBxNa8/ObdaPJD
+UXx1FHgVQKIGmXZQxK03zpQPtopiPu6nwq7kHcRANbt7bNYVQg32Ye1hkVDuAmnbAOxHcG
+ssWHQhyliCZ5lDm6RNXSzeQGEHeaxwQDbkdAXQI2e1Uqd1TJdhmlADmumeD2HRUuAvRs6N
+4wDUY6lPeSFW+wYAZvexDGF8hugYOswAtUAoMCGRs3dU1GiBtucQ4JcNbCMeujzAFA6k57
+G8yZ0gShfl4SG9Ka460A7xuuBf79gJ0ECzQomRNBDuwCRaVRDgXpKPDHeAZ0BOAh0oBlzJ
+tbV53P3JJE+N5pCleWiBmBkbOrMzQLpinOU2AmQ3JddUoXOUyd4+HdQE1hZIaxzRNDilQC
++dyzjpRDR6gTrScgBh/uj1Z0tqaEQLkr39+GAhxvoF02iUPUx9iMR84xCDBgyDCECMkQjN
+aAfn5tMcW/sQDrwOtsJ1ou9V+NwuHiKl0TRO45ajqHJLlWCGKokmjHAAccqfxdyRThQkG8
+ludUA=
 '''
 import ui
 import bz2
@@ -75,10 +75,10 @@ v = ui.load_view_str(pyui.decode('utf-8'))
 
 v.name = '$aveUp^'
 v['Transactions'].hidden = True
-v['Congradulations'].hidden = True
-v['Congradulations']['imageview1'].image = ui.Image.named('IMG_0417.PNG')
-#v['Congradulations'].hidden = False
-#v['Congradulations'].bring_to_front()
+v['Congratulations'].hidden = True
+v['Congratulations']['imageview1'].image = ui.Image.named('IMG_0417.PNG')
+#v['Congratulations'].hidden = False
+#v['Congratulations'].bring_to_front()
 
 #v['meetinggoalsegment'].action = segmented_action
 #v['button1'].action = segmented_action
