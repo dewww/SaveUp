e3d559de76d33ddc583d5bd0633fe346ee143203
Dewayne Cowles <dewww@users.noreply.github.com>
2018-10-21 10:43:30 -0700

    Final Changes

--- SaveUp.py
+++ SaveUp.py
@@ -8,22 +8,23 @@ def weekly_total_action(sender):
 #	v['view1'].hidden = True
 	v['Transactions'].bring_to_front()
 	v['Transactions'].hidden = False
-	v['Transactions'].present('popover',hide_close_button=False)
+#	v['Transactions'].present('popover') #,hide_close_button=False)
 	
 def txn_detail_action(sender):
-	sender.tableview.superview['tableview1'].hidden = True
-	sender.tableview.superview['tableview1'].send_to_back()
-	sender.tableview.superview['tableview2'].hidden = True
-	sender.tableview.superview['tableview2'].send_to_back()
-	sender.tableview.superview['textview1'].hidden = False
-	sender.tableview.superview['textview1'].bring_to_front()
+#	sender.tableview.superview['tableview1'].hidden = True
+#	sender.tableview.superview['tableview1'].send_to_back()
+#	sender.tableview.superview['tableview2'].hidden = True
+#	sender.tableview.superview['tableview2'].send_to_back()
+#	sender.tableview.superview['textview1'].hidden = False
+#	sender.tableview.superview['textview1'].bring_to_front()
+	reset_view(sender)
 			
 def sweep_action(sender):
 	v['view1'].send_to_back()
 #	v['view1'].hidden = True
 	v['Congratulations'].bring_to_front()
 	v['Congratulations'].hidden = False
-	v['Congratulations'].present('popover',hide_close_button=False)
+	v['Congratulations'].present('popover') #,hide_close_button=False)
 	
 def reset_view(sender):
 	v['view1'].bring_to_front()
@@ -33,12 +34,20 @@ def reset_view(sender):
 	v['Congratulations'].send_to_back()
 	v['Congratulations'].hidden = True
 
+def hide_splash(sender):
+	sender.hidden=True
+	sender.send_to_back()
+	v['view1'].bring_to_front()
+	v['view1'].hidden = False
+
 def segmented_action(sender):
 #	if sender.segments[sender.selected_index] == 'meetinggoalsegment':
 	sender.selected_index = 0 #reset 
 	v['view1']['saveuptosegment'].selected_index += 1
 	if v['view1']['saveuptosegment'].selected_index <= 0:
 		v['view1']['imageview1'].hidden = True	
+		v['view1']['imageview1'].image = ui.Image.named('CreditScore.PNG')
+		v['view1']['imageview1'].hidden = False
 	else:
 		v['view1']['imageview1'].hidden = True	
 		v['view1']['imageview1'].image = ui.Image.named('IMG_042'+format(v['view1']['saveuptosegment'].selected_index)+'.PNG')
@@ -47,54 +56,55 @@ def segmented_action(sender):
 
 		
 data = '''\
-QlpoOTFBWSZTWUWL/r0ADNbf4VVQVGd/9b/v3/6/79/+QAAAIBAAYAs/PRrGQGObbssADo
-GaldtjQAoAMJJEyIyYg1MaJlPUZkhkxNGNQAZDEAAaEGgE0xCkaCA0GgANAAaaAABxkaZM
-TQZMmE0yBkNAaA0yaGAE0BhJqIiRRvKmZR5GoND1DTajE9RmkepoxANGnqBocZGmTE0GTJ
-hNMgZDQGgNMmhgBNAYRKE0CNJkTEyNPVT0JoeoA9TEbT0KD1BkNPUF1dgYqP6ooKesA2fv
-nx30rBtPEIDUIGBtLhW0YwS5AkW6sRSqsVuyZDj5KYrMzIQWChERBCChIJEJMccwADOKIo
-GY/CSDFSAQICRSBIEiBBioAeClVKlSoKJVT1bZoWPtKDSASQjCMAoFHGKA5kA9nJwzkDh1
-CQklx82vOU3M/XR8OWSP0l4uWqtVOmSKH9/u2n2lh1PV/Tz54AnaqESnbt+T18+OrzdiuD
-tfP1dj0zVr9pEiXWLwCFswlkvEoIhxwzcMJFAxLoumCkQUdgxFqpIFjIUUENIQtKCHXA6u
-Xv8KWFyZopJOSRvtONBhMEUk4U8hVLA6MNu3cdC5vPp7IFtxxey3y8iRnc93K1Ny7XeoO7
-ajfsxkMZlLPRqv1xWIoyGhgxnBVG5nE7q/UkPcYKgrAggKQUUFFUgoRBgIyRghGC9+IyM+
-nwh0STE8WR0qCmDBnossSJEREXXExMg4hK0GtRkJrrqUjqGAoiAkZpNQUiwdc4VYRvTSSa
-1LNTQqii5kkzVZMMzKkdaLLE1uKCwWWWgQ3pDkqCv+ULOWGydkyejd8dhUQLCgqEQiSSit
-FaVih7CcSBUioenuo/JBIQBwzBPNbtxfltldw0fmc8JTIccqGA5Fq2vYyhjYwagXybkDcq
-GznG8N18KOVMd9l0jbaldKL0HANJvfmBdgRlG7fTufqsz45BHHDC6SiWSrX0ebbBKQi44P
-jGwSrE9gR2hTYMSW1kpPwOFjPw/ELgDBkYBgOwd26y9rt0ollV1m6rroLt/M5ufEJrw10H
-SAjRDU3E4Ow27M+rDx/ZJ4lZnWjxORRNAfn2xHYmkCi0iDUME0vIIIQArv2qpqXeS8/BRd
-oLS/eeVGow5arI2zUQofBaEFaDhDfYeTMV5KqpiSt8RZTAM6DJz7hus6ZQ14tOjLBpjMuo
-7bYD8DKh3ULVHSr11TXQsb4Vywrxwom0YU0yvpgpv7B4VcB4XIunvOmteYbTcyvlhu5HPv
-6rbAhMWU2XWsMSmDOMkMuUIcNJHaje8JKIFhWBnU2lWNIQBGYh1HsnZxLR0AjW2aBgbWcn
-UrO2SwAHScm6c3SIdmVm2ytpgZzG9SAGIu17QoI7wqCoTJEScLNemwD675F+iqylAVCYRe
-KCDBi+bTy0TMX60NSdp40LhrsJThLA4HvLM2w2557HdGBESpXUXqdDNARk+/PZ02+jBLja
-CK0KAFBR6jRpbLFoQFk0pFgZoPZiN9n2AwsbVSp2zfJ8XYrQ+NQQbqdiD1c3UxZd1PFMnV
-NrqOHPhGxwTjAjVM0ptvFNVmY3SbtbIVgWA9CbKfEUboxZzobpvLRkU+JcWpbZ2JKyLqRz
-N1VR6VWB2GzoVCwaC2mK4aUTCzQcku70RssJfEauZLld9mSFaQgXOMSmRCG+asPeCPeYKO
-8nYcmyEuH4RRIQbHFaJqMa1BSng1dWVHBRhzInDGxye54S6VIMK7wMEQ5BEQITGKAoiEEJ
-EgKQjDnwbxZOonFpW/JBbTZcP41zVp5LgG85s1B2dRk11EU5TXpgjZO/6Plmf4C3ioLeYD
-6WGNa1nnEWcYbU2O56jHrqJvPaa167duX+Ugtu63BnIJuYbj6EHj2AqK5xNuqriHGWwVqO
-tFhPicg9NOoatPvp5AL9vNthvzC58JneMHHnfszlsXdSZoVmDWap2jXr9kIX1umN8dm8Rd
-myxPdsWnNy5DnB+rPXw6Q06832r1LudznWqJfHOOMx1p9hdy14vKyzZdHQVvd0oj2KSLfr
-M06W9k7712UOuMCl/Ejz6mHjxQmuNDYzvX0/KkxeDWkT1M/te9kE/DxjKyrDFSjIsc7uF4
-CXYVWP4JQOnf6rOb16nupogqFINBfBwJXaNue4YCHSxwTaaPvXu4H7OIi2mL2wX+ZqQORS
-DauV2MOHt1wkRnbE1Iz5tmd/Lv6+seNh5mB2TOfq008krVKQsaEFiIlVLWvT5fs+8kPgl1
-YxVLMXYOO58Ph63vZefoaEsXPfLG/CG2TZ1Yg6kk3VZaZjO3e2hq0wUTQMHmE/gzJRoVIe
-ghy+XZ7+SHdCbSNd0MyjPPE/0SJ6AocUMDo9WXwt7pivSoXuo8lQln126ITUjSDw93mhEB
-GMR5E17I7p7kRsCoEUTXx9djKZ4eHIfc8kgcU9iiF/0BFYVsONIWu660yyiobuCdnGxbxB
-uCYgyFhyNVCwr7GlJKtlPRcoam/cCVqYnd2HLZJEdf9mnLlEJDYgo/FSuQZHMKkIeVMTyQ
-lcqJYg55qSIIfAEOVA5K/u9dqnMBMFRmBlcFQYjQIN8wBMjjiBt1nQZbxeUNTHXSFqh1lg
-SgJwpmZ++pYzLenrtCd4j2UB0Xx+Qp5n5lDU6gXqIqFA4vghNw1Qi331G3sYV69YaGlg6g
-Hh4KFj4qHUvfvMXFegxe+aWN+3CmpMYgc9L59HPzd3Sc9C9tVM+NqMCnYQGHKgyEOwm8Zg
-qFOvp1Nk8MwSjcOdSp8oOgHEtvSoJqddhpYPSPBq3UA793l5LAKWb5n3CDSugBVSEUhT7y
-mDhfesBLljV/HccYa4XDVccgTEsSBghYKY2ELDAtQtmb93TznXuPS12BOOZ5gxTAOEiQiR
-gfij5BCgeVGPFLeFNlbcJ1DfTPza2EOgUY8lbuuIpbtMZS1Ez2DkqH9uB+E5c8wcc/f08m
-5mA7y+FQzbFsyw1qNM2cBThBJFpVRPKZU9WHHDStLxO7W+XlUPRN+ADn3qUgkwVC5jL1sd
-Y7goYdAOO7mkkGfOdu1KSFLW2KSh31VN4mWpqbfgzLpY3gmQVJBsORXYBzAriUxB88MpUw
-ORjmHkHLpxKgOoYjHEdBHhc0yKbEx17RzUNdDPniZuMTdwojwM4HoAdPypTZQv/3By8+6t
-b6UCpBHEpS3k44WBuRTYaTedefAMDwZoPJTdge8IHvVivtAikAgwZBjCBGSISMCeuJgAn0
-fv5ZZN/aq9UAzwfoC4P0q/YGk/j4ntPsGlmBae6i09xNQ13XykmIaG9iiUYmCmNger1fF/
-4u5IpwoSCLF/16
+QlpoOTFBWSZTWXGVohgADXBf4VVQVOd/9b/v3/6/79/+QAAAIBAAYAufAKKADI0sADLQmr
+2wAAFABhomgmhMj00npBNNNT1HpD1NNBiPUaDR6nqNqeoABoIACYSFJoeoAGgAAaaAGgAA
+5hNAaA0aMI0GI0xMmJoMI0DIBkwICUAAAAAAAAAAAAANDmE0BoDRowjQYjTEyYmgwjQMgG
+TARJEaICZTAIaKaNqZT9RPT1T9Jp6jTaaUY0Yo8mkM1eIMVH2RVU+cA4v5T899WgXj7wgN
+ggZBkFBaLGDkwJFyAIANgCAHAAyfL30hJkGSMgjJJAiJIIyAkIhIYxqqAawEEQ1H+0kGKk
+AgQEikCQJASDFRA8VLKWLFlRSynr4aiXPvKGoBJGMIwCgpxAQqCFpTRKEorcIgHze31eY4
+a+b+u7/n2Xz/vj9vs49AA+SSKH3fhwn3l12nr+3z65InagkRV3hr6PFgoPmhToYx8ck34h
+9ioNXaRGCMIpaLBIoAzJZtgtdoGB2plZLwZBLtqLFQ0hUGBISiMISzayCUQGikTMUtCkTm
+B+P6fmeqwWUrI/dc9kNv/cKoZI4AKHdc6OzXP0Wy2XKWyerusv9rRioUK4PFBBZqHVOzCK
+dnHhoHDh6yHLHIZnLTt9FPl6dmp6IGj2d9GI9k3Hf/gU8YkkCSIJAJEZCQJCQkAkBgRCEV
+iQWJJ+4IRY/P9Ae4Au/wyJmUkhduwhBgwhCEmeYe4Mgl2MErMlYIC4xMSMwF0oYBBjjDGQ
+ZEmMryKwtkAYwFjBmVRRZyFyqjIvHJpZimxYhizIEiSBZtEU1U1pJJ+dEjZ9z6k1fEwffx
++SwUwCwUFAEBgAUAUAVaAnrJzkgWIgnq6qT40gQgjlqiea/bhxeZN9X43TKVgcXoxQwtj1
+YuWhi5hshnpWZErcgnB1jeG7PKnSsb7rsjfhVtlLzOJdv59oHChFpw93H0sP1llrwUCOPL
+4bWyUS1nNNaBKPi04RfGwZVU9ko1vGtwqTbZKdzAlkPzPjHAAyZGAYBuE4cLFt8350Niq3
+S1V0QJOX2nx+26OOmvceZCDSG/cbycQsMG/Zr03+v2ye8tNbU8jmUmwH1cMDwbo63UvUS7
+PcaWQL7+XhbEwzjs8+3PPPZx8Mh5idhCF16LcXFPbM6pbizAhAgNYGkGgeYhdfpmC8CZj3
+HSM5Gjr3jms6d1WN3Jro1ya0mvWdt8h+00ojal1s9Vk2alwrvrnxrhFhWzGhsyU39qcbOQ
+VdLg5Kb1ST3LEcQsGJHMPiuRLMQ5JAMofE5VypOQkUgPJBkQMooQ6MJG7jjEJPEC09S7TU
+hWNHvBQRB2izrOyk+jYBGMswDQvZqalZt0sQA68qGzObZEO2nzxeDK5GoR4sCAyyM9+6Ki
+WI2BVJZhOXqCfOHV0RB1saHHUrLaoCGKkg9KCZMT3aemCZjjrsbk3bhscBgblKdEdYJA8V
+npkb89mdDJ4jWOm9TedzNARkLjt16q3jnCAoUvFOJNEiAiQ51KzCGH8TaFIrGlpKNh2pYw
+24GoTvVKnfjg+bq5gbCgIbbKpBz2M1MVfw88UyeLLxuA5a+M53OKZi1NTnfEEpTkxfC7q/
+gyuCeDJZcuptoYw5HhcR1lBOOK5uyDXMk/fLaRRyTfA6i7YbD4MGr0FmEUTE4Gg6AKCdty
+KrZaTjMqNWbuh4sshakIFvOZTIhDGNWYeYo9xkJLBUshK3FMIwRC5zWibhClAVZ5N7UrKi
+h4y2LIZubJOcororelnozDmp1oKm0gSASEIiQFghICxPmuanwbj+I9uOHLsTTju6b8zHXf
+HZvzzrpLocDevMW75jNlXVWBzpDsoD5RXy/Q/Ch/iLenUX8XjANTjGMcMHoLILvGjMMnvl
+J5tOuh16WWvC3cQdVjXAznC0wYbnxIPXuRVFdZ2w7Oy/nTMPYjbRUnROROTdHPYflJ84HH
+f6Wv2PPWOMp0vvVJNN466ruTZNk81bsUmiQnBO+N+DtGHG8Jm2e2IjhbvieIRZhKOjHQer
+gzcr1dHuohp18f2w60n3u90tzVManlJZjnmwgx1Obxq/leEbB7G+R3EHbyY4/1kOghpu0I
+W+lTzAJ3uWg4CzniyZ5s1Gr2endUI9zvEQorNN6IZAUa+cGVtdjScDIWqV9QWkO1Cu1OVw
+A/4fp2/jc9e+nxiGYVAlBbp6YV4HHL5C6J9MPk09FP3wZrE7/MGnnahwwdB1ADyEsHo8qp
+Aa9+piyGU4mowZfs0y2ZTZjae6J9kQ7jLL588/qaxCoFiUQJGEAgMxQiPvqy6QEQlihoBC
+REQICkQAJVKZfbSIWDCS5zVIjm4GM2L1bYgbQAD224xkGU8+wi5ONhNGYGB2Ax+BktGZQP
+3CW/T8NPTmJ8oPAZXyhk0Zexj+JIniFHIcjo/20/tf4TC+RQzzBfIgku+2/RlpwJcsHw9u
+Waidgn7Wb+yvd9yZ9S8UTdpyLdHVJDLp6cg988FDseihLfkCwkStDtglpONqzyZSCbuKdn
+K5f3g5omEZC6GhtBLqv82qks3U8buZLG063hvRL3NTvzOfAIQXd/k2c+cWQkeJAF/MG2ga
+HoLEIdyYOqEtpSXIO7Z4cDCJbNEp3lLy839e93G00DieAR1HcdJ5xDfMkTQ5YAp4dZ0Gm8
+XnDaabdYXsvWXRKRONamv2WLmpf9HXeTvEeygdi/V8RXe+V/UobjqReoiCUHN84zeNkIuf
+Cw3+lh3X7d8Np/d3GQdgLz6AS54B+ah2LXEw4XmYOE2XOHHoraTEEPTsz15enzeHSenY53
+2g68r0wK7YMgw50MhDtJvGeOaCW7Onc8Uz1RKcw9Nix+oHYByL70sCbTruNXX4g4lnMUDy
+7u7msAq7nqfgINW2AllLtNKQt+RV3LPesRMy5tflzHENuWYbVxoiYLkgZIXCsXULjAvRfU
+37vJ6Z17j4m3BE5anlDCZLxkGMSDA+SPUoUHcnZ043jp3TrNrl8bLeHqglZFMrr6tzQThP
+U1wxkJkMCBAEN9Y1CGd4BOf2WQJoeC7zPKwaty+pddje422M4inGCSIVYAe41r15csttqz
+id+3PTuUPGb8gG018AagklkEzMTO2R1juWjLkDjd6Ekgz1Hbwva0ha/AylHhYB3iabTacP
+TqZpc3omgWJBuOhbgC6gWwVhHzQ0lnJ5mNV6hNPJgsC7QwscCbBXjmbNCuBMbe4TUE27DX
+0YNXETdxpHiaxfEF2fsAo4Amf/uTp591s77IVLEEcFVbq5ZXBzIpwGpvOvXiGR5mbB5g7s
+j7AgfYAQAs/WhSkQgxJFjCBGSCSBAnzxLCJ9H8uemgZ/WPNiD0wTZm/QGAf4AH1Btn/XvP
+rPqGrsC8+FAV8M5W0Nu7GpJoE2GwzATBQqMhIkEAWWnfxdyRThQkHGVohgA=
 '''
 import ui
 import bz2
@@ -105,11 +115,17 @@ v = ui.load_view_str(pyui.decode('utf-8'))
 # ------------------------
 
 v.name = '$aveUp^'
-v['imageview1'].image = ui.Image.named('SelectSite.png')
+v['view1'].hidden=False
+#v['view2'].send_to_back()
+#v['view2'].hidden=True
+#v['view2']['button1'].image = ui.Image.named('SelectSite.png')
+#v['view2']['button1'].image = ui.Image.named('IMG_0420.PNG')
+v['view2'].hidden=True
+#v['view2'].hidden=False
 v['Transactions'].hidden = True
 v['Congratulations'].hidden = True
 v['Congratulations']['imageview1'].image = ui.Image.named('IMG_0417.PNG')
-v['view1']['imageview1'].image = ui.Image.named('IMG_0420.PNG')
+#v['view1']['imageview1'].image = ui.Image.named('IMG_0420.PNG')
 print('IMG_042'+format(v['view1']['saveuptosegment'].selected_index)+'.PNG')
 #v['Congratulations'].hidden = False
 #v['Congratulations'].bring_to_front()
